bad7d4c3553078cc52feb3c1057883763b68e388
Jordan Alexander <jalexander@radford.edu*>
2013-11-22 23:33:54 -0500

    Object Oriented Engine
    Re-designed the entire javascript files to be completely object oriented
    
    Split into 3 classes:
    
    game engine, which initiates the player object and elements object.

--- vampire.js
+++ vampire.js
@@ -1,242 +1,208 @@
-//variables to keep track of player values, counters and other game values
-//values change often
-var count = 0;
-var bloodCount = 0;
-var multiplier = 1;
-var dayStatus;
-var goldCount = 0;
-var hp = 20;
-var hpMax = 20;
+function _elements() {
+	this.counter;
+	this.blood;
+	this.gold;
+	this.spanHP;
+	this.divHP;
+	this.bloodElement;
+	this.raidElement;
+	this.msg;
+	this.goldDiv;
+	this.day;
+	this.cycle;
+	this.goHunting;
+	this.elm = function(name,props,style) {
+		var el = document.createElement(name);
+		for(var prop in props) if(props.hasOwnProperty(prop)) el[prop] = props[prop];
+		for(var prop in style) if(style.hasOwnProperty(prop)) el.style[prop] = style[prop];
+		return el;
+	}
+	this.showElement = function(id) {
+		document.getElementById(id).className = "";
+	}
+	this.disableElement = function(id,txt) {
+		document.getElementById(id).disabled = true;
+		document.getElementById(id).innerHTML = txt;
+	}
+	this.alterHTML = function(id,txt) {
+		document.getElementById(id).innerHTML = txt;
+	}
+	this.enableButton = function(id,e,txt) {
+		var element = document.getElementById(id);
+		if (engine.player.isDead() && engine.dayStatus != "night") 
+			engine.elements.eventMsg("You are too weak to "+e+" until pure darkness allows it!");
+		else {
+			element.disabled = false;
+			this.alterHTML(id,txt);
+		}
+	}
+	this.addBorder = function(id) {
+		document.getElementById(id).style.border = "1px solid black";
+	}
+	this.eventMsg = function(txt) {
+		this.addBorder("msg");
+		var temp = document.getElementById("msg");
+		txt = "-"+txt+"<br />"+temp.innerHTML;
+		temp.innerHTML = txt;
+	}
+	this.bloodButton = function() { 
+		this.goHunting = this.elm("button",{innerHTML:"Hunt for Blood", id:"bloodButton"},{}); 
+		document.body.appendChild(this.goHunting);
+		this.goHunting.addEventListener("click",engine.player.hunt.bind());
+	}
+	this.raidButton = function() { 
+		this.goRaiding = this.elm("button",{innerHTML:"Raid for Gold", id:"raidButton"},{}); 
+		document.body.appendChild(this.goRaiding);
+		this.goRaiding.addEventListener("click",engine.player.raid.bind());
+	}
+}
 
-//flags for initial functions that shouldn't repeat
-//(button appending for example)
-//death is reoccurring
-var cycleFlag = false;
-var raidFlag = false;
-var dead = false;
-var firstHPLoss = false;
+function _player() {
+	this.hp = 20;
+	this.hpMax = 20;
+	this.bloodcount = 0;
+	this.goldCount = 0;
+	this.isDead = function() {
+		if (this.hp <= 0) return true;
+		else return false;
+	}
+	this.revive = function() {
+		if (engine.dayStatus == "night") {
+			engine.player.healDamage(1);
+		}
+	}
+	this.healDamage = function(heal) {
+		if ((this.hp+heal) > this.hpMax) this.hp = this.hpMax;
+		else this.hp += heal;
+	}
+	this.triggerDeath = function(cause,bloodLoss) {
+		engine.elements.eventMsg("You have died from: "+cause);
+		engine.elements.eventMsg("Your death has cost you "+bloodLoss+" pints of your precious blood!");
+		if (this.bloodcount < 20) this.bloodcount = 0;
+		else this.bloodcount -= bloodLoss;
+		engine.elements.alterHTML("blood",this.bloodcount);
+	}
+	this.dealDamage = function(dmg,type,bloodLossOnDeath) {
+		if (!engine.firstHPLoss) {
+			engine.firstHPLoss = true;
+			engine.elements.showElement("hpDiv");
+		}
+		if ((this.hp-dmg) <= 0) {
+			this.hp = 0;
+			this.triggerDeath(type,bloodLossOnDeath);
+		} else {
+			this.hp -= dmg;
+		}
+	}
+	this.hunt = function() {
+		var bloodCollected = 0;
+		engine.elements.alterHTML("bloodButton","Wait to hunt...");
+		engine.elements.goHunting.disabled = true;
+		if (engine.dayStatus == engine.statusCycle[0]) {
+			this.dealDamage(10,"sunlight",20);
+			engine.elements.eventMsg("Hunting in the daylight has hurt you! -10 HP!");
+		} else {
+			bloodCollected = 1*engine.multiplier;
+			engine.player.bloodcount += bloodCollected;
+			engine.elements.alterHTML("blood",engine.player.bloodcount);
+			engine.elements.eventMsg("Your hunt yielded "+bloodCollected+" pint(s) of blood!");
+			engine.player.healDamage(1);
+			engine.elements.alterHTML("hp",engine.player.hp);
+		}
+	}
+	this.raid = function() {
+		engine.elements.showElement("goldDiv");
+		var goldCollected;
+		var hpLoss = 0;
+		engine.elements.alterHTML("raidButton","Wait to raid...");
+		engine.elements.goRaiding.disabled = true;
+		if (engine.dayStatus == engine.statusCycle[0]) {
+			engine.player.dealDamage(15,"sunlight");
+			engine.elements.eventMsg("Raiding in the daylight has hurt you! -15 HP!");
+		} else {
+			hpLoss = Math.floor(Math.random()*(5-1+1)+1);
+			goldCollected = Math.floor((Math.random()*100));
+			engine.player.goldCount += goldCollected;
+			engine.elements.alterHTML("gold",engine.player.goldCount);
+			engine.elements.eventMsg("Your raid yielded "+goldCollected+" gold coins at the cost of "+hpLoss+"HP from the townspeople!");
+			engine.player.dealDamage(hpLoss,"raiding",15);
+			engine.elements.alterHTML("hp",engine.player.hp);
+		}
+	}
+}
 
-//uninitialized button variables
-//dynamically created and assigned
-var goHunting; 
-var goRaiding;
+function _engine() {
+	this.count = 0;
+	this.cycleFlag = false;
+	this.firstHPLoss = false;
+	this.raidFlag = false;
+	this.multiplier = 1;
+	this.dayStatus = "dusk";
+	this.statusCycle = [
+		"day",
+		"dusk",
+		"night",
+		"dawn"
+	];
+	this.huntStatus = {
+		"day":0,
+		"dusk":3,
+		"night":4,
+		"dawn":2
+	};
+	this.dayFlavor = [
+		"The sun is bright outside..",
+		"The sun is setting..",
+		"The moon shines brightly..",
+		"The sun is rising.."
+	];
+	this.player = (function(){ return new _player(); }());
+	this.elements = (function(){ return new _elements(); }());
+	this.triggers = function(c) {
+		if (c == 5) this.elements.bloodButton();
+		if (!(c%1) && c > 5) this.elements.enableButton("bloodButton","hunt","Hunt for Blood");
+		if (!(c%1) && engine.player.bloodcount > 5) this.elements.enableButton("raidButton","raid","Raid for Gold");
+		if (engine.player.bloodcount >= 5 && !this.raidFlag) {
+			this.elements.raidButton();
+			this.raidFlag = true;
+		}
+		if (engine.player.bloodcount >= 10 && !this.cycleFlag) {
+			this.initDayCycle();
+			this.cycleFlag = true;
+		}
+		if (!(c%10) && this.cycleFlag) this.nextDayCycle();
+	}
+	this.initDayCycle = function() {
+		engine.elements.showElement("divCycle");
+		engine.elements.alterHTML("cycle",this.dayStatus);
+		engine.multiplier = 3;
+	}
+	this.nextDayCycle = function() {
+		var index = this.statusCycle.indexOf(this.dayStatus);
+		var cycleNext = this.statusCycle[(index+1)];
+		if (cycleNext) {
+			this.dayStatus = cycleNext;
+			this.multiplier = this.huntStatus[cycleNext];
+		}
+		else {
+			this.dayStatus = this.statusCycle[0];
+			this.multiplier = this.huntStatus[this.dayStatus];
+		}
+		engine.elements.alterHTML("cycle",this.dayStatus);
+		var newIndex = this.statusCycle.indexOf(this.dayStatus);
+		engine.elements.eventMsg(this.dayFlavor[newIndex]);
+	}
+}
 
-//html element variables
-var counter = document.getElementById("counter");
-var blood = document.getElementById("blood");
-var gold = document.getElementById("gold");
+var engine = (function(){ return new _engine(); }());
 
-//objects and arrays for day statuses and cycles
-var statusCycle = [
-	"day",
-	"dusk",
-	"night",
-	"dawn"
-];
-var huntStatus = {
-	"day":0,
-	"dusk":3,
-	"night":4,
-	"dawn":2
-};
-var dayFlavor = [
-	"The sun is bright outside..",
-	"The sun is setting..",
-	"The moon shines brightly..",
-	"The sun is rising.."
-];
-
-//initial interval
-//this shit controls the game!
 setInterval(function() { 
-	count++;
-	counter.innerHTML = count;
-	triggers(count);
+	engine.count++;
+	engine.elements.alterHTML("counter",engine.count);
+	engine.triggers(engine.count);
+	if (engine.player.isDead()) {
+		engine.elements.disableElement("bloodButton","You are dead..");
+		engine.elements.disableElement("raidButton","You are dead..");
+		engine.player.revive();
+	}
 }, 1000);
-
-//lose 20 pints of blood when you die
-//add proper event log messages
-//set dead flag for other functions to know you're dead
-function triggerDeath(cause) {
-	var bloodloss = 20;
-	addEventMsg("You have died from: "+cause);
-	addEventMsg("Your death has cost you "+bloodloss+" of your precious blood!");
-	if (bloodCount < 20) bloodCount = 0;
-	else bloodCount -= bloodloss;
-	blood.innerHTML = bloodCount;
-	dead = true;
-}
-
-//if player is dead, revive to 1HP during the night cycle only
-//else heal player by designated amount, maxing out at players capped maxHP
-function healDamage(heal) {
-	if (dead) {
-		hp = 1;
-		addEventMsg("The powerful of the nights moon revives you enough to resume hunting.");
-		dead = false;
-	} else {
-		if ((hp+heal) > hpMax) hp = hpMax;
-		else hp += heal;
-		document.getElementById("hp").innerHTML = hp;
-	}
-}
-
-//first time losing HP - remove hidden class from the div!
-//subtract damage dealt
-//call death function appropriately, passing in what killed you
-//update div to proper HP value
-function dealDamage(dmg,type) {
-	if (!firstHPLoss) {
-		firstHPLoss = true;
-		document.getElementById("hpDiv").className = "";
-	}
-	if ((hp-dmg) <= 0) {
-		hp = 0;
-		triggerDeath(type);
-	} else {
-		hp -= dmg;
-	}
-	document.getElementById("hp").innerHTML = hp;
-}
-
-//this function is a life saver
-//properly appends the event log from top to bottom.
-function addEventMsg(txt) {
-	var msg = document.getElementById("msg");
-  	msg.style.border = "1px solid black";
-	txt = "-"+txt+"<br />"+msg.innerHTML;
-	msg.innerHTML = txt;
-}
-
-//dynamic appending of the raid button when called
-function raidButton() {
-	goRaiding = elm("button",{innerHTML:"Raid for Gold",id:"raidButton"},{});
-	document.body.appendChild(goRaiding);
-	goRaiding.addEventListener("click",raid.bind());
-}
-
-//dynamic appending of the hunt button when called
-function bloodButton() { 
-	goHunting = elm("button",{innerHTML:"Hunt for Blood", id:"bloodButton"},{}); 
-	document.body.appendChild(goHunting);
-	goHunting.addEventListener("click",hunt.bind());
-}
-
-//enables buttons when triggered
-//if dead, will only enable when called during night cycle
-//set html properly
-function enableButton(bid,bEvent,newTxt) {
-	var element = document.getElementById(bid);
-	if (dead && dayStatus != "night") addEventMsg("You are too weak to "+bEvent+" until pure darkness allows it!");
-	else {
-		element.disabled = false;
-		element.innerHTML = newTxt;
-	}
-}
-
-//go hunting son!
-//disable button
-//add message and deal damage if hunting in the daylight
-//collect yo blood and heal 1HP from drankin it
-function hunt() {
-	var btxt = document.getElementById("bloodButton");
-	var bloodCollected;
-	btxt.innerHTML = "Wait to hunt..";
-	goHunting.disabled = true;
-	if (dayStatus == statusCycle[0]) {
-		dealDamage(10,"sunlight");
-		addEventMsg("Hunting in the daylight has hurt you! -10 HP!");
-	} else {
-		bloodCollected = 1*multiplier;
-		bloodCount += bloodCollected;
-		blood.innerHTML = bloodCount;
-		addEventMsg("Your hunt yielded "+bloodCollected+" pint(s) of blood!");
-		healDamage(1);
-	}
-}
-
-//holy shit we're going raiding.
-//same shit as the hunt, just with gold
-//why?
-//cause raiding.
-function raid() {
-	var goldDiv = document.getElementById("goldDiv").style.display = "block";
-	var rtxt = document.getElementById("raidButton");
-	var goldCollected;
-	var hpLoss = 0;
-	goRaiding.innerHTML = "Wait to raid..";
-	goRaiding.disabled = true;
-	if (dayStatus == statusCycle[0]) {
-		dealDamage(15,"sunlight");
-		addEventMsg("Raiding in the daylight has hurt you! -15 HP!");
-	} else {
-		hpLoss = Math.floor(Math.random()*(5-1+1)+1); //random math 1-5 HP loss cause people fight back
-		goldCollected = Math.floor((Math.random()*100)); //random coinage 1-100 like a boss
-		goldCount += goldCollected;
-		gold.innerHTML = goldCount;
-		addEventMsg("Your raid yielded "+goldCollected+" gold coins at the cost of "+hpLoss+"HP from the townspeople!");
-		healDamage(1);
-	}
-}
-
-//bad ass element creation function thanks to @BenjaminGruenbaum on SO JS chat
-//you rock bro
-function elm(name,props,style){
-	var el = document.createElement(name);
-	for(var prop in props) if(props.hasOwnProperty(prop)) el[prop] = props[prop];
-	for(var prop in style) if(style.hasOwnProperty(prop)) el.style[prop] = style[prop];
-	return el;
-}
-
-
-//start cycling through the days
-//initial values only!
-//set dat *er
-function initDayCycle() {
-	var day = document.getElementById("divCycle").style.display = "block";
-	dayStatus = "dusk";
-	var cycle = document.getElementById("cycle");
-	cycle.innerHTML = dayStatus;
-	multiplier = 3;
-}
-
-//change day cycle into the next value
-function changeDayCycle() {
-	var index = statusCycle.indexOf(dayStatus);
-	var cycleNext = statusCycle[(index+1)];
-	if (cycleNext) {
-		dayStatus = cycleNext;
-		multiplier = huntStatus[cycleNext];
-	}
-	else {
-		dayStatus = statusCycle[0];
-		multiplier = huntStatus[dayStatus];
-	}
-	cycle.innerHTML = dayStatus;
-	var newIndex = statusCycle.indexOf(dayStatus);
-	addEventMsg(dayFlavor[newIndex]);
-}
-
-//THE SECOND MOST IMPORTANT FUNCTION
-//these triggers control EVERYTHING
-//triggered by the interval (trigger the triggers ha!)
-function triggers(count) {
-	if(count == 5) bloodButton();
-	if(!(count % 5) && count > 5) {
-		enableButton("bloodButton","hunt","Hunt for Blood");
-	}
-	if(!(count % 10) && bloodCount > 35) {
-		enableButton("raidButton","raid","Raid for Gold");
-	}
-	if (bloodCount >= 10 && !cycleFlag) {
-		initDayCycle();
-		cycleFlag = true;
-	}
-	if (bloodCount >= 35 && !raidFlag) {
-		raidButton();
-		raidFlag = true;
-	}
-	if (!(count%10) && cycleFlag) {
-		changeDayCycle();
-	}
-}
