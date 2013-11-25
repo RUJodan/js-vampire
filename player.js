5e8f956e19e53351af672729791b36ef12ad1179
Jordan Alexander <jordana@rainn.org>
2013-11-25 17:18:53 -0500

    Player Prototype Object
    Implements the prototype inheritence of the engine object.
    
    Better functional separation by using more parameters in functions and more functions for repetative tasks

--- player.js
+++ player.js
@@ -7,70 +7,70 @@ function _player() {
 
 _player.prototype = {
 	isDead : function() {
-		if (this.hp <= 0) return true;
+		if (engine.player.hp <= 0) return true;
 		else return false;
 	},
 	revive : function() {
 		if (engine.dayStatus == "night") {
-			this.healDamage(1);
+			engine.player.healDamage(1);
 		}
 	},
 	healDamage : function(heal) {
-		if ((this.hp+heal) > this.hpMax) this.hp = this.hpMax;
-		else this.hp += heal;
-		engine.elements.alterHTML("hp",this.hp);
+		if ((engine.player.hp+heal) > engine.player.hpMax) engine.player.hp = engine.player.hpMax;
+		else engine.player.hp += heal;
+		engine.elements.alterHTML("hp",engine.player.hp);
 	},
 	triggerDeath : function(cause,bloodLoss) {
 		engine.elements.eventMsg("You have died from: "+cause);
 		engine.elements.eventMsg("Your death has cost you "+bloodLoss+" pints of your precious blood!");
-		if (this.bloodcount < 20) this.bloodcount = 0;
-		else this.bloodcount -= bloodLoss;
-		engine.elements.alterHTML("blood",this.bloodcount);
+		if (engine.player.bloodcount < 20) engine.player.bloodcount = 0;
+		else engine.player.bloodcount -= bloodLoss;
+		engine.elements.alterHTML("blood",engine.player.bloodcount);
+		engine.elements.disableElement("bloodButton","Wait to Hunt...");
+		engine.elements.disableElement("raidButton","Wait to Raid...");
 	},
 	dealDamage : function(dmg,type,bloodLossOnDeath) {
 		if (!engine.firstHPLoss) {
 			engine.firstHPLoss = true;
-			engine.elements.showElement("hpDiv");
+			engine.elements.showElement("hpDiv","block");
 		}
-		if ((this.hp-dmg) <= 0) {
-			this.hp = 0;
-			this.triggerDeath(type,bloodLossOnDeath);
+		if ((engine.player.hp-dmg) <= 0) {
+			engine.player.hp = 0;
+			engine.player.triggerDeath(type,bloodLossOnDeath);
 		} else {
-			this.hp -= dmg;
+			engine.player.hp -= dmg;
 		}
+		engine.elements.alterHTML("hp",engine.player.hp);
 	},
 	hunt : function() {
 		var bloodCollected = 0;
-		engine.elements.alterHTML("bloodButton","Wait to hunt...");
-		engine.elements.disableElement("bloodButton");
+		engine.elements.disableElement("bloodButton","Wait to Hunt...");
 		if (engine.dayStatus == engine.statusCycle[0]) {
-			this.dealDamage(10,"sunlight",20);
+			engine.player.dealDamage(10,"sunlight",20);
 			engine.elements.eventMsg("Hunting in the daylight has hurt you! -10 HP!");
 		} else {
 			bloodCollected = 1*engine.multiplier;
-			this.bloodcount += bloodCollected;
-			console.log(bloodCollected);
-			engine.elements.alterHTML("blood",this.bloodcount);
+			engine.player.bloodcount += bloodCollected;
+			engine.elements.alterHTML("blood",engine.player.bloodcount);
 			engine.elements.eventMsg("Your hunt yielded "+bloodCollected+" pint(s) of blood!");
-			this.healDamage(1);
+			engine.player.healDamage(1);
 		}
 	},
 	raid : function() {
-		engine.elements.showElement("goldDiv");
+		engine.elements.showElement("goldDiv","block");
 		var goldCollected;
 		var hpLoss = 0;
-		engine.elements.alterHTML("raidButton","Wait to raid...");
-		engine.elements.disableElement("raidButton");
+		engine.elements.disableElement("raidButton","Wait to Raid...");
 		if (engine.dayStatus == engine.statusCycle[0]) {
-			this.dealDamage(15,"sunlight");
+			engine.player.dealDamage(15,"sunlight",25);
 			engine.elements.eventMsg("Raiding in the daylight has hurt you! -15 HP!");
 		} else {
 			hpLoss = Math.floor(Math.random()*(5-1+1)+1);
 			goldCollected = Math.floor((Math.random()*100));
-			this.goldCount += goldCollected;
-			engine.elements.alterHTML("gold",this.goldCount);
+			engine.player.goldCount += goldCollected;
+			engine.elements.alterHTML("gold",engine.player.goldCount);
 			engine.elements.eventMsg("Your raid yielded "+goldCollected+" gold coins at the cost of "+hpLoss+"HP from the townspeople!");
-			this.dealDamage(hpLoss,"raiding",15);
+			engine.player.dealDamage(hpLoss,"raiding",15);
 		}
 	}
 }
