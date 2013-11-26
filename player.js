88fa0dfc9281527bcc36680b4f615fed15ea0688
Jordan Alexander <jalexander@radford.edu*>
2013-11-26 00:30:15 -0500

    Minor Bug Fix

--- player.js
+++ player.js
@@ -47,12 +47,12 @@ _player.prototype = {
 		engine.elements.disableElement("bloodButton","Wait to Hunt...");
 		if (engine.dayStatus == engine.statusCycle[0]) {
 			engine.player.dealDamage(10,"sunlight",20);
-			engine.elements.eventMsg("Hunting in the daylight has hurt you! -10 HP!");
+			engine.elements.eventMsg("Hunting in the daylight has hurt you! <span>-10</span> HP!");
 		} else {
 			bloodCollected = 1*engine.multiplier;
 			engine.player.bloodcount += bloodCollected;
 			engine.elements.alterHTML("blood",engine.player.bloodcount);
-			engine.elements.eventMsg("Your hunt yielded "+bloodCollected+" pint(s) of blood!");
+			engine.elements.eventMsg("Your hunt yielded <span>"+bloodCollected+"</span> pint(s) of blood!");
 			engine.player.healDamage(1);
 		}
 	},
@@ -63,13 +63,13 @@ _player.prototype = {
 		engine.elements.disableElement("raidButton","Wait to Raid...");
 		if (engine.dayStatus == engine.statusCycle[0]) {
 			engine.player.dealDamage(15,"sunlight",25);
-			engine.elements.eventMsg("Raiding in the daylight has hurt you! -15 HP!");
+			engine.elements.eventMsg("Raiding in the daylight has hurt you! <span>-15</span> HP!");
 		} else {
 			hpLoss = Math.floor(Math.random()*(5-1+1)+1);
 			goldCollected = Math.floor((Math.random()*100));
 			engine.player.goldCount += goldCollected;
 			engine.elements.alterHTML("gold",engine.player.goldCount);
-			engine.elements.eventMsg("Your raid yielded "+goldCollected+" gold coins at the cost of "+hpLoss+"HP from the townspeople!");
+			engine.elements.eventMsg("Your raid yielded <span>"+goldCollected+"</span> gold coins at the cost of "+hpLoss+"HP from the townspeople!");
 			engine.player.dealDamage(hpLoss,"raiding",15);
 		}
 	}
